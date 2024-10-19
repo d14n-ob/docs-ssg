76619dc87312767a89eba74f48315abef9f76ca3
Lin Yun <sutiankongy@gmail.com>
2024-10-19 15:20:35 +0800

    init

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+import withMarkdoc from '@markdoc/next.js'
+
+import withSearch from './src/markdoc/search.mjs'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  pageExtensions: ['js', 'jsx', 'md', 'ts', 'tsx'],
+}
+
+export default withSearch(
+  withMarkdoc({ schemaPath: './src/markdoc' })(nextConfig),
+)
